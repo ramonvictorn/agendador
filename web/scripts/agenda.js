14efe50f0f4d2a022946c3b533631686a3dffff5
Ramon Victor <ramonvictorn@gmail.com>
2018-09-14 17:28:42 -0300

    calendar semi

--- web/scripts/agenda.js
+++ web/scripts/agenda.js
@@ -5,23 +5,39 @@ $(function() {
     // page is now ready, initialize the calendar...
   
     $('#calendar').fullCalendar({
+      //config for new design
+      // editable: false, // Don't allow editing of events
+      handleWindowResize: true,
+      weekends: false, // Hide weekends
+     
+      header: true, // Hide buttons/titles
+      minTime: '07:00:00', // Start time for the calendar
+      maxTime: '22:00:00', // End time for the calendar
+      displayEventTime: true, // Display event time
+      resources: 'https://fullcalendar.io/demo-resources.json',
+      //finish designaddResource
+      selectable:true,
         slotDuration: '00:30:00',
         slotLabelInterval:'01:00',
-        defaultView: 'agendaWeek',
         header: {
             left: 'prev,next today',
             center: 'title',
             right: 'month,agendaWeek,agendaDay,listMonth'
           },
+          defaultView: 'agendaWeek', // Only show week view
+          
           locale: 'pt-br',
           timeFormat: 'H(:mm)',
           displayEventEnd: true,
           weekNumbers: true,
-          eventLimit: true, // allow "more" link when too many events
+          selectOverlap:false,
+          eventLimit: true, // allow "more" link when too mfalseany events
           events: [
             {
               title  : 'reserva 1',
-              start  : '2018-09-14T12:30:00'
+              start  : '2018-09-14T12:30:00',
+              description: 'description for All Day Event',
+              color: '#C2185B'
             },
             {
               title  : 'reserva 2',
@@ -34,27 +50,54 @@ $(function() {
               end : '2018-09-14T19:30:00'
             }
           ],
-          height: 600,
+          height: 500,
           weekends:false,
           timezone: 'America/Sao_Paulo',
           //Quando envento click no evento
-          eventClick: function(eventObj) {
-            if (eventObj.url) {
-              alert(
-                'Clicked ' + eventObj.title + '.\n' +
-                'Will open ' + eventObj.url + ' in a new tab'
-              );
+          // eventClick: function(eventObj) {
+          //   if (eventObj.url) {
+          //     alert(
+          //       'Clicked ' + eventObj.title + '.\n' +
+          //       'Will open ' + eventObj.url + ' in a new tab'
+          //     );
       
-              window.open(eventObj.url);
+          //     window.open(eventObj.url);
       
-              return false; // prevents browser from following link in current tab.
-            } else {
-              alert('Evento ' + eventObj.title + ' Ás ' + eventObj.start);
-            }
+          //     return false; // prevents browser from following link in current tab.
+          //   } else {
+          //     console.log('Evento ' + eventObj.title + ' Ás ' + eventObj.start._i, eventObj);
+          //     console.log('Evento as ' , eventObj.start)
+          //   }
+          // },
+
+          dayClick: function(date) {
+            console.log('clicked ' + date.format());
           },
+          select: function(startDate, endDate,eventObj) {
+            console.log('selected ' + startDate.format() + ' to ' + endDate.format());
+            
+          },
+          selectOverlap: function(event) {
+            console.log('ja tem evento cadastrado neste horário')
+            return event.rendering === 'background';
+          },
+          eventClick: function(calEvent, jsEvent, view) {
+
+            alert('Event: ' + calEvent.title);
+            alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+            alert('View: ' + view.name);
+        
+            // change the border color just for fun
+            $(this).css('border-color', 'red');
+        
+          }
+         
+          
           
           
     });
+
+    
 });
 
 
---
+++ web/scripts/agendaScript.js
@@ -0,0 +1,23 @@
+console.log('deu')
+
+
+function inserirBtAdd(){
+    // $('.fc-header-toolbar').append('<button>Reservar Sala </button');
+
+
+}
+  
+
+
+var a = $('.fc-button-group');
+console.log(a)
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    inserirBtAdd();
+    
+});
+
+function tt(){
+
+}
