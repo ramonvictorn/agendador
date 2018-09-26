e0774ed2adc83228cc5465e4a8725531036d46c6
Ramon Victor <ramonvictorn@gmail.com>
2018-09-26 17:22:16 -0300

    interacoes separadas

--- web/scripts/agenda.js
+++ web/scripts/agenda.js
@@ -4,94 +4,32 @@ $(function() {
 
     // page is now ready, initialize the calendar...
   
-    $('#calendar').fullCalendar({
-      editable: false, // Don't allow editing of events
-      handleWindowResize: true,
-      weekends: false, // Hide weekends
-      header: true, // Hide buttons/titles
-      minTime: '07:00:00', // Start time for the calendar
-      maxTime: '22:00:00', // End time for the calendar
-      displayEventTime: true, // Display event time
-      resources: 'https://fullcalendar.io/demo-resources.json',
-      //finish designaddResource
-      selectable:true,
-        slotDuration: '00:30:00',
-        slotLabelInterval:'01:00',
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay,listMonth'
-          },
-          defaultView: 'agendaWeek', // Only show week view
-          
-          locale: 'pt-br',
-          timeFormat: 'H(:mm)',
-          displayEventEnd: true,
-          weekNumbers: true,
-          selectOverlap:false,
-          eventLimit: true, // allow "more" link when too mfalseany events
-          events: [
-            {
-              title  : 'reserva 1',
-              start  : '2018-09-25T16:30:00',
-              end  : '2018-09-25T18:30:00',
-              description: 'Reunião com adm',
-              color: '#C2185B'
-            },
-            {
-              title  : 'reserva 2',
-              start  : '2018-09-25T18:30:00',
-              end : '2018-09-25T19:30:00',
-              description: 'Reunião com dev',
-              color: '#Cdcdcd'
-            },
-            {
-              title  : 'reserva 3',
-              start  : '2018-09-25T19:30:00',
-              end : '2018-09-25T21:30:00',
-              color: 'pink'
-            }
-          ],
-          height: 500,
-          weekends:false,
-          timezone: 'America/Sao_Paulo',
-        
-        // Events Mouse
-          dayClick: function(date) {
-            console.log('clicked ' + date.format('DD/MM/YYYY HH:mm:ss'));
-            $('.modalCadastro').show();
-            $('.modalCadastro').css('opacity', 1);
-           
-          },
-
-          // Alguma data é selecionada
-          select: function(startDate, endDate,eventObj) {
-            console.log('selected aqui ' + startDate.format() + ' to ' + endDate.format());
-            var inicio = $('.cadastroInicio');
-            inicio.val(startDate.format('DD/MM/YYYY HH:mm:ss'));
-            $('.cadastroFim').val(endDate.format('DD/MM/YYYY HH:mm:ss'))
-            console.log('inicio ', inicio)
-            $('.modalCadastro').show();
-          },
-
-          // Quando é selecionado um horário com evento
-          selectOverlap: function(event) {
-            console.log('ja tem evento cadastrado neste horário')
-            $('.modalPequena').show(); //mostrar a modal
-            $('.pequenaTittle').text("Reserva não é possivel");
-            return event.rendering === 'background';
-          },
-
-          // Click sobre evento
-          eventClick: function(calEvent, jsEvent, view) {
-            $('.modalPequena').show(); //mostrar a modal
-            $('.pequenaTittle').text(calEvent.title);
-            $('.inicio').text(calEvent.start.format('DD/MM/YYYY HH:mm:ss'));
-            $('.fim').text(calEvent.end.format('DD/MM/YYY HH:mm:ss'));
-            $('.titulo').text(calEvent.description);
-            $('.pequenaBody').css('background-color', calEvent.color)
-            console.log('start: ',calEvent.start.format('DD/MM/YYYY HH:mm:ss'))
-          },       
+  $('#calendar').fullCalendar({
+    editable: false, // Don't allow editing of events
+    handleWindowResize: true,
+    weekends: false, // Hide weekends
+    header: true, // Hide buttons/titles
+    minTime: '07:00:00', // Start time for the calendar
+    maxTime: '22:00:00', // End time for the calendar
+    displayEventTime: true, // Display event time
+    selectable:true,
+    height: 500,
+    weekends:false,
+    timezone: 'America/Sao_Paulo',
+    slotDuration: '00:30:00',
+    slotLabelInterval:'01:00',
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay,listMonth'
+    },
+    defaultView: 'agendaWeek', 
+    locale: 'pt-br',
+    timeFormat: 'H(:mm)',
+    displayEventEnd: true,
+    weekNumbers: true,
+    selectOverlap:false,
+    eventLimit: true, 
           
     });
 
---
+++ web/scripts/interacao.js
@@ -0,0 +1,98 @@
+$( document ).ready(function() {
+    clickDia();
+    selectDay();
+    clickEvento();
+    selecionadoJaExiste();
+})
+
+function clickDia(){
+    console.log('[interacao.js] - clickDia');
+    $('#calendar').fullCalendar('option', 'dayClick', function(date) {
+            console.log('[interacao.js] - evento - Click ' + date.format('DD/MM/YYYY HH:mm:ss'));
+            $('.modalCadastro').show();
+            $('.modalCadastro').css('opacity', 1);
+           
+        });
+}
+
+function selectDay(){
+    console.log('[interacao.js] - selectDay');
+    $('#calendar').fullCalendar('option', 'select', function(startDate, endDate,eventObj) {
+        console.log('interacao.js - evento - selec');
+            var inicio = $('.cadastroInicio');
+            inicio.val(startDate.format('DD/MM/YYYY HH:mm:ss'));
+            $('.cadastroFim').val(endDate.format('DD/MM/YYYY HH:mm:ss'))
+            console.log('inicio ', inicio)
+            $('.modalCadastro').show();
+        });
+}
+
+function clickEvento(){
+    console.log('[interacao.js] - clickEvento');
+    $('#calendar').fullCalendar('option', 'eventClick', function(calEvent, jsEvent, view) {
+            console.log('[interacao.js] Evento -> Click sobre evento');
+            $('.modalPequena').show(); //mostrar a modal
+            $('.pequenaTittle').text(calEvent.title);
+            $('.inicio').text(calEvent.start.format('DD/MM/YYYY HH:mm:ss'));
+            $('.fim').text(calEvent.end.format('DD/MM/YYY HH:mm:ss'));
+            $('.titulo').text(calEvent.description);
+            $('.pequenaBody').css('background-color', calEvent.color)
+            $('.usuario').text(calEvent.user)
+        });
+}
+
+
+function selecionadoJaExiste(){
+    console.log('[interacao.js] - selecionadoJaExiste');
+    $('#calendar').fullCalendar('option', 'selectOverlap', function(event) {
+            console.log('[interacao.js] Evento -> Selecionado Já Existe');
+            $('.modalPequena').show(); //mostrar a modal
+            $('.pequenaTittle').text("Reserva não é possivel");
+            return event.rendering === 'background';
+        });
+}
+
+
+
+
+
+
+
+
+// BACKUP NÃO APAGAR------------------------------
+
+// dayClick: function(date) {
+//     console.log('clicked ' + date.format('DD/MM/YYYY HH:mm:ss'));
+//     $('.modalCadastro').show();
+//     $('.modalCadastro').css('opacity', 1);
+   
+// },
+
+// select: function(startDate, endDate,eventObj) {
+//     console.log('selected aqui ' + startDate.format() + ' to ' + endDate.format());
+//     var inicio = $('.cadastroInicio');
+//     inicio.val(startDate.format('DD/MM/YYYY HH:mm:ss'));
+//     $('.cadastroFim').val(endDate.format('DD/MM/YYYY HH:mm:ss'))
+//     console.log('inicio ', inicio)
+//     $('.modalCadastro').show();
+// },
+
+//  // Click sobre evento
+//  eventClick: function(calEvent, jsEvent, view) {
+//     $('.modalPequena').show(); //mostrar a modal
+//     $('.pequenaTittle').text(calEvent.title);
+//     $('.inicio').text(calEvent.start.format('DD/MM/YYYY HH:mm:ss'));
+//     $('.fim').text(calEvent.end.format('DD/MM/YYY HH:mm:ss'));
+//     $('.titulo').text(calEvent.description);
+//     $('.pequenaBody').css('background-color', calEvent.color)
+//     $('.usuario').text(calEvent.user)
+//     console.log(calEvent, 'start: ',calEvent.start.format('DD/MM/YYYY HH:mm:ss'))
+//   },   
+
+// Quando é selecionado um horário com evento já cadastrado
+// selectOverlap: function(event) {
+//     console.log('ja tem evento cadastrado neste horário')
+//     $('.modalPequena').show(); //mostrar a modal
+//     $('.pequenaTittle').text("Reserva não é possivel");
+//     return event.rendering === 'background';
+//   },
