039e77ed81d7429c68306c1cef27e602ebc9df5c
Ramon Victor <ramonvictorn@gmail.com>
2018-11-07 16:54:37 -0200

    protecao dados rota insertEvent

--- core/controllers/insertEvent.js
+++ core/controllers/insertEvent.js
@@ -2,8 +2,31 @@ exports.insertEvent = insertEvent;
 const insertEventModel = require('../models/insertEvent');
 
 function insertEvent(req,res){
-    console.log('insert event', req.body)
-    var evento= req.body;
+    console.log('Controller insert event')
+    var evento = {};
+    evento.name = req.body.name;
+    evento.start = req.body.start;
+    evento.end = req.body.end;
+    evento.finalidade = req.body.finalidade;
+    evento.usuario = req.body.usuario;
+
+    if(evento.name == null || evento.name == undefined || evento.name == ""){
+        return res.status(401).send({error:"Parametros_Invalidos"})
+    }
+    if(evento.start == null || evento.start == undefined || evento.start == ""){
+        return res.status(401).send({error:"Parametros_Invalidos"})
+    }
+    if(evento.end == null || evento.end == undefined || evento.end == ""){
+        return res.status(401).send({error:"Parametros_Invalidos"})
+    }
+    if(evento.finalidade == null || evento.finalidade == undefined || evento.finalidade == ""){
+        return res.status(401).send({error:"Parametros_Invalidos"})
+    }
+    if(evento.usuario == null || evento.usuario == undefined || evento.usuario == ""){
+        return res.status(401).send({error:"Parametros_Invalidos"})
+    }
+
+
     if(validarEvento(evento)){
         insertEventModel.insertEvent(evento,(eventoInserido)=>{
             console.log('sucess in insert')
