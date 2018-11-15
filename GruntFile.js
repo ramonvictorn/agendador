33e44f716310ad1decdf93b29aaebb0162af7f4a
Ramon Victor <ramon.victor.n.com>
2018-11-15 19:41:56 -0200

    version 1 build

---
+++ GruntFile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+  'string-replace': {
+    inline: {
+      files: {
+        'dist/': 'web/view/*',
+      },
+      options: {
+        replacements: [{
+          pattern: /localhost:8080/gm,
+          replacement: '150.162.6.19:500'
+        }]
+      }
+    }
+  },
+  uglify: {
+    my_target: {
+      files: {
+        'dist/web/js/agenda.min.js': ['web/scripts/agenda/*'],
+        'dist/web/js/login.min.js': ['web/scripts/login/*']
+      }
+    }
+  },
+  copy: {
+    main: {
+      files: [
+        // includes files within path
+        {expand: true,flatten: true ,src: ['web/css/**'], dest: 'dist/web/css/', filter: 'isFile'},
+  
+      ],
+    },
+  },
+  processhtml: {
+    build: {
+        files: {
+            'dist/web/view/login.html' : ['dist/web/view/login.html'],
+        }
+    }
+}
+  });
+
+  grunt.loadNpmTasks('grunt-string-replace');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-processhtml');
+
+
+  grunt.registerTask('default', ['string-replace','uglify','copy','processhtml']);
+
+};
