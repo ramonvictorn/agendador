a3e5b813cbd3740b7a1eefefa240b1ad29e8056f
Ramon Victor <ramonvictorn@gmail.com>
2019-02-20 20:52:44 -0300

    router pages ok

--- web/private/index.js
+++ web/private/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-// import LoginView from '../private/js/views/LoginView.jsx'
+import LoginView from '../private/js/views/LoginView.jsx'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
-// import NoMatch404 from '../private/js/views/NoMatch404.jsx';
+import NoMatch404 from '../private/js/views/NoMatch404.jsx';
 import AgendaView from '../private/js/views/AgendaView.jsx';
 import { Provider } from 'react-redux';
 import store from './js/store.js'
@@ -13,7 +13,13 @@ import './css/agenda.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <AgendaView></AgendaView>
+        <Router>    
+            <Switch>
+                <Route path='/login' component={LoginView}/>
+                <Route path='/agenda' component={AgendaView}/>
+                <Route component={NoMatch404}/>
+            </Switch>
+        </Router>
     </Provider>
     ,
     document.getElementById("app")
