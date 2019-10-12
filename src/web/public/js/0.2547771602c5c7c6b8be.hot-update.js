2794ae4faa392eb6fef4b313a77363d96c92c803
Ramon <ramonvictorn@gmail.com>
2019-10-11 21:51:35 -0300

    trash files removeds

--- src/web/public/js/0.2547771602c5c7c6b8be.hot-update.js
+++
@@ -1,7 +0,0 @@
-webpackHotUpdate(0,{23:
-/*!***********************************************************************************************!*\
-  !*** ./node_modules/css-loader/dist/cjs.js!./src/web/private/js/components/SideBar/style.css ***!
-  \***********************************************************************************************/
-/*! no static exports found */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(module,exports,__webpack_require__){eval('exports = module.exports = __webpack_require__(/*! ../../../../../../node_modules/css-loader/dist/runtime/api.js */ 5)(false);\n// Module\nexports.push([module.i, ".side-bar{\\n    width: 20%;\\n}", ""]);\n\n\n\n//# sourceURL=webpack:///./src/web/private/js/components/SideBar/style.css?./node_modules/css-loader/dist/cjs.js')}});
